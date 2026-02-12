91c037c74615d48c812c8bc5d4150298e798aaac
robNice <robnice@downlord.net>
2026-02-12 18:33:33 +0100

    Refactor ReleaseDialog to replace individual radio buttons with a bound buttonsGroup

--- src/main/kotlin/de/robnice/deploybutler/version/ReleaseDialog.kt
+++ src/main/kotlin/de/robnice/deploybutler/version/ReleaseDialog.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.builder.AlignX
+import com.intellij.ui.dsl.builder.bind
 import de.robnice.deploybutler.i18n.message
 import javax.swing.*
 
@@ -12,51 +13,47 @@ class ReleaseDialog(
     private val currentVersion: Version
 ) : DialogWrapper(project) {
 
-    private val revisionButton = JRadioButton(
-        message("dialog.release.revision", "v${currentVersion.bump(ReleaseType.REVISION)}")    )
-
-    private val featureButton = JRadioButton(
-        message("dialog.release.feature", "v${currentVersion.bump(ReleaseType.FEATURE)}")    )
-
-    private val majorButton = JRadioButton(
-        message("dialog.release.major", "v${currentVersion.bump(ReleaseType.MAJOR)}")    )
+    private var selectedType: ReleaseType = ReleaseType.REVISION
 
     init {
-        ButtonGroup().apply {
-            add(revisionButton)
-            add(featureButton)
-            add(majorButton)
-        }
         title = message("dialog.release.title")
-        revisionButton.isSelected = true
         init()
     }
 
     override fun createCenterPanel(): JComponent {
 
         return panel {
-            row {
-                cell(revisionButton)
-                label("v${currentVersion.bump(ReleaseType.REVISION)}")
-                    .align(AlignX.RIGHT)
-            }
-            row {
-                cell(featureButton)
-                label("v${currentVersion.bump(ReleaseType.FEATURE)}")
-                    .align(AlignX.RIGHT)
-            }
-            row {
-                cell(majorButton)
-                label("v${currentVersion.bump(ReleaseType.MAJOR)}")
-                    .align(AlignX.RIGHT)
-            }
+            buttonsGroup {
+                row {
+                    radioButton(
+                        message("dialog.release.revision",
+                            "v${currentVersion.bump(ReleaseType.REVISION)}"),
+                        ReleaseType.REVISION
+                    )
+
+                }
+                row {
+                    radioButton(
+                        message("dialog.release.feature",
+                            "v${currentVersion.bump(ReleaseType.FEATURE)}"),
+                        ReleaseType.FEATURE
+                    )
+
+                }
+                row {
+                    radioButton(
+                        message("dialog.release.major",
+                            "v${currentVersion.bump(ReleaseType.MAJOR)}"),
+                        ReleaseType.MAJOR
+                    )
+
+                }
+            }.bind(
+                { selectedType },
+                { selectedType = it }
+            )
         }
     }
 
-    fun getSelectedType(): ReleaseType =
-        when {
-            revisionButton.isSelected -> ReleaseType.REVISION
-            featureButton.isSelected -> ReleaseType.FEATURE
-            else -> ReleaseType.MAJOR
-        }
+    fun getSelectedType(): ReleaseType = selectedType
 }
